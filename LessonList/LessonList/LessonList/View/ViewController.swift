fc07fe66b63aea2a27dadb9285af1aa8f3f2a8bd
Omoyeni <babatundeabike3@gmail.com>
2023-01-04 09:04:26 +0100

    project_setup_modeling_into_the_mvvm_architecture

---
+++ LessonList/LessonList/LessonList/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonList
+//
+//  Created by Sharon Omoyeni Babatunde on 04/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
