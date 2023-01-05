c51497cb2f590fdb74887785fac1c2161b6bc169
Omoyeni <babatundeabike3@gmail.com>
2023-01-05 16:19:24 +0100

    constraints_created_with_NSLayout

---
+++ LessonList/LessonList/Utilities/Extensions/UIView+Layout.swift
@@ -0,0 +1,120 @@
+//
+//  UIView+Layout.swift
+//  LessonList
+//
+//  Created by Sharon Omoyeni Babatunde on 04/01/2023.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @discardableResult
+    func anchor(top: NSLayoutYAxisAnchor? = nil, leading: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, trailing: NSLayoutXAxisAnchor? = nil, margin: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        
+        if let top = top {
+            anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: margin.top)
+        }
+        
+        if let leading = leading {
+            anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: margin.left)
+        }
+        
+        if let bottom = bottom {
+            anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -margin.bottom)
+        }
+        
+        if let trailing = trailing {
+            anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -margin.right)
+        }
+        
+        if size.width != 0 {
+            anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width)
+        }
+        
+        if size.height != 0 {
+            anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height)
+        }
+        
+        [anchoredConstraints.top, anchoredConstraints.leading, anchoredConstraints.bottom, anchoredConstraints.trailing, anchoredConstraints.width, anchoredConstraints.height].forEach{ $0?.isActive = true }
+        
+        return anchoredConstraints
+    }
+ 
+    func placeAtLeftCenterOf(centerY: NSLayoutYAxisAnchor? = nil, leading: NSLayoutXAxisAnchor? = nil, verticalSpace: CGFloat = 0, margin: UIEdgeInsets = .zero, size: CGSize = .zero){
+        translatesAutoresizingMaskIntoConstraints = false
+        if let centerY = centerY {
+            centerYAnchor.constraint(equalTo: centerY).isActive = true
+        }
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: margin.left).isActive = true
+        }
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        if let centerY = centerY {
+            centerYAnchor.constraint(equalTo: centerY, constant: verticalSpace).isActive = true
+        }
+    }
+    
+    func placeAtRightCenterOf(centerY: NSLayoutYAxisAnchor? = nil, trailing: NSLayoutXAxisAnchor? = nil, margin: UIEdgeInsets = .zero, size: CGSize = .zero){
+        translatesAutoresizingMaskIntoConstraints = false
+        if let centerY = centerY {
+            centerYAnchor.constraint(equalTo: centerY).isActive = true
+        }
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -margin.right).isActive = true
+        }
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+    
+    func placeAtCenterOf(centerY: NSLayoutYAxisAnchor? = nil, centerX: NSLayoutXAxisAnchor? = nil, verticalSpace: CGFloat = 0, horizontalSpace: CGFloat = 0, size: CGSize = .zero){
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        if let centerY = centerY {
+            centerYAnchor.constraint(equalTo: centerY, constant: verticalSpace).isActive = true
+        }
+        if let centerX = centerX {
+            centerXAnchor.constraint(equalTo: centerX, constant: horizontalSpace).isActive = true
+        }
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        
+    }
+        
+    func constrainWidth(constant: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: constant).isActive = true
+    }
+    
+    func constrainHeight(constant: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: constant).isActive = true
+    }
+
+    func addSubviews(_ views:UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
+
+
+struct AnchoredConstraints {
+    var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+}
+
