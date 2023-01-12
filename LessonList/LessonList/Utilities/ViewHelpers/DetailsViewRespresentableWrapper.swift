488e5256432e850a32e82edc0d06598bd31c0bb1
Omoyeni <babatundeabike3@gmail.com>
2023-01-12 12:20:28 +0100

    uikit_uiviewControllerReoresentable_class_to_wrap_uikit_from_swift_ui_tap_gesture

---
+++ LessonList/LessonList/Utilities/ViewHelpers/DetailsViewRespresentableWrapper.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsViewRespresentablerapper.swift
+//  LessonList
+//
+//  Created by Sharon Omoyeni Babatunde on 11/01/2023.
+//
+
+import SwiftUI
+
+struct DetailsViewRespresentableWrapper: UIViewControllerRepresentable {
+    
+    var movieList: (lesson: StoredLessonModel, arrayOfLesson: [StoredLessonModel], totalCount: Int, currentCount: Int)
+    var lessonListViewModel: LessonListViewModel? = nil
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DetailsViewRespresentableWrapper>) -> UIViewController {
+        
+        ControllerSceneDIContainer.sharedInstance.movieList = movieList
+        ControllerSceneDIContainer.sharedInstance.lessonListViewModel = lessonListViewModel
+        return ControllerSceneDIContainer.sharedInstance.makeDetailPageInjectable()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<DetailsViewRespresentableWrapper>) {
+    }
+}
