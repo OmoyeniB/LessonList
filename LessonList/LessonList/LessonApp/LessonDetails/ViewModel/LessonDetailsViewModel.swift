f5a16c629a9eb6e39654abe00ddb608cc79befc2
Omoyeni <babatundeabike3@gmail.com>
2023-01-12 12:46:17 +0100

    lessonlist_viewmodel_creates_functions_to_supply_results_when_it_is_needed_by_the_controller

---
+++ LessonList/LessonList/LessonApp/LessonDetails/ViewModel/LessonDetailsViewModel.swift
@@ -0,0 +1,91 @@
+//
+//  LessonDetailsViewModel.swift
+//  LessonList
+//
+//  Created by Sharon Omoyeni Babatunde on 08/01/2023.
+//
+
+import Foundation
+import LinkPresentation
+import AVKit
+
+protocol LessonDetailProtocol: AnyObject {
+    func streamVideo(url: String)
+}
+
+final class LessonDetailsViewModel {
+    
+    var willStreamVideo: ((_ player: AVPlayer, _ playController: AVPlayerViewController) -> (Void))?
+    var onDismiss: (() -> ())?
+    weak var delegate: LessonDetailProtocol?
+    let videoDownloader = VideoDownloader()
+    var updateUI: (() -> Void)?
+   
+    var alreadyDownloadedID = [Int]() {
+        didSet {
+            saveDownloadedID()
+        }
+    }
+    var downloadState = false {
+        didSet {
+            saveDownloadedState()
+        }
+    }
+    
+    func saveDownloadedState() {
+        UserDefaults.standard.set(downloadState, forKey: "downloadedState")
+    }
+    
+    func saveDownloadedID() {
+       
+        if #available(iOS 12.0, *) {
+            do {
+                let data = try NSKeyedArchiver.archivedData(withRootObject: alreadyDownloadedID, requiringSecureCoding: true)
+                UserDefaults.standard.set(data, forKey: "alreadyDownloadedID")
+            } catch {
+                print(error)
+            }
+        } else {
+            let data = NSKeyedArchiver.archivedData(withRootObject: alreadyDownloadedID)
+            UserDefaults.standard.set(data, forKey: "alreadyDownloadedID")
+        }
+    }
+    
+    
+    func getDataFromUserDefault() {
+        if let data = UserDefaults.standard.object(forKey: "alreadyDownloadedID") as? Data {
+            let array = NSKeyedUnarchiver.unarchiveObject(with: data) as? [Int]
+            alreadyDownloadedID = array ?? [Int]()
+        }
+    }
+    
+    func isDownloaded(id: Int) -> String {
+        getDataFromUserDefault()
+        if alreadyDownloadedID.contains(id) {
+            return "Downloaded"
+        } else {
+            return "Download"
+        }
+    }
+    
+    func playVideo_Stream_Video(urlString: String) {
+        let docsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let destinationUrl = docsUrl.appendingPathComponent("\(urlString).mp4")
+        
+        if (FileManager().fileExists(atPath: destinationUrl.path)) {
+            let baseUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            print("found oo")
+            let assetUrl = baseUrl.appendingPathComponent("\(urlString).mp4")
+            let url = assetUrl
+            print(url)
+            let avAssest = AVAsset(url: url)
+            let playerItem = AVPlayerItem(asset: avAssest)
+            let player = AVPlayer(playerItem: playerItem)
+            let playerViewController = AVPlayerViewController()
+            playerViewController.player = player
+            willStreamVideo?(player, playerViewController)
+        } else {
+            delegate?.streamVideo(url: urlString)
+        }
+    }
+}
