1408d724775780f33d063de232e65a57163461ca
Omoyeni <babatundeabike3@gmail.com>
2023-01-13 13:32:14 +0100

    video_cache_class_updated_to_query_filemanager_before_trying_to_save_again

--- LessonList/LessonList/Utilities/CustomClass/VideoCache.swift
+++ LessonList/LessonList/Utilities/CustomClass/VideoCache.swift
@@ -13,8 +13,8 @@ protocol VideoCacheProtocol {
 }
 
 class VideoCache {
-
-   let videoDownloader = VideoDownloader()
+    
+    let videoDownloader = VideoDownloader()
     static let shared = VideoCache()
     private var delegate: VideoCacheProtocol?
     private let cachedMetadata = NSCache<NSURL, LPLinkMetadata>()
@@ -23,19 +23,54 @@ class VideoCache {
     }
     
     final func load(url: NSURL, completion: @escaping ((LPLinkMetadata?) -> Void)) {
+        if  hasSavedUrl(url: url) {
+            let savedMetadata = self.getMetadataFromSavedUrl(url: url)
+            completion(savedMetadata)
+            return
+        }
         if let cachedData = metaData(url: url) {
             completion(cachedData)
             return
         }
         let metaProvider = LPMetadataProvider()
-        metaProvider.startFetchingMetadata(for: (url as URL)) {[weak self] metaData, error in
+        metaProvider.startFetchingMetadata(for: (url as URL)) { [weak self] metaData, error in
             completion(metaData)
-            if let metaData {
+            if let metaData = metaData{
                 self?.cachedMetadata.setObject(metaData, forKey: url)
+                let data = NSKeyedArchiver.archivedData(withRootObject: metaData)
+                self?.saveMetaData(url: url, data: data)
             } else {
-               completion(nil)
+                completion(nil)
                 self?.delegate?.didShowError(error: error!.localizedDescription)
             }
         }
+    }
+    
+    func saveMetaData(url: NSURL, data: Data) {
+        let filePath = getFilePath(url: url)
+        FileManager.default.createFile(atPath: filePath, contents: data, attributes: nil)
+    }
+    
+    func hasSavedUrl(url: NSURL) -> Bool {
+        let filePath = getFilePath(url: url)
+        return FileManager.default.fileExists(atPath: filePath)
+    }
+    
+    func getMetadataFromSavedUrl(url: NSURL) -> LPLinkMetadata? {
+        let filePath = getFilePath(url: url)
+        if FileManager.default.fileExists(atPath: filePath) {
+            let data = FileManager.default.contents(atPath: filePath)
+            if let data {
+                let metadata = NSKeyedUnarchiver.unarchiveObject(with: data) as? LPLinkMetadata
+                return metadata
+            }
+        }
+        return nil
+    }
+    
+    func getFilePath(url: NSURL) -> String {
+        let fileName = url.lastPathComponent ?? ""
+        let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return documentsUrl.appendingPathComponent(fileName).path
     }
 }
